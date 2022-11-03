c7d193f31c51d6d6673d4bf60056914de78cef7f
MUHAMMAD AHMED HASHMI <57319953+mahmed-eng@users.noreply.github.com>
2022-11-04 00:28:57 +0500

    Update app.js

--- Sec project/app.js
+++ Sec project/app.js
@@ -1,78 +1,78 @@
-var f = prompt("Welcome !" + "\n" + "Press (1) to generate an Array" + "\n" + "Press (2) to generate a Marksheet")
-if (f === "1") {
-    var a = prompt("Enter Your element to an array: ")
-    var b = prompt("Enter Your element to an  array: ")
-    var c = prompt("Enter Your element to an  array: ")
-    var d = prompt("Enter Your element to an  array: ")
-    var e = [a, b, c, d]
-    alert('Array becomes: ' + e)
-} else if (f === "2") {
-    var s = prompt("Enter Your Name: ")
-    alert("Name: " + s)
-    var p = prompt("Enter Your Roll No: ")
-    alert("Rollno: " + p)
+// var f = prompt("Welcome !" + "\n" + "Press (1) to generate an Array" + "\n" + "Press (2) to generate a Marksheet")
+// if (f === "1") {
+//     var a = prompt("Enter Your element to an array: ")
+//     var b = prompt("Enter Your element to an  array: ")
+//     var c = prompt("Enter Your element to an  array: ")
+//     var d = prompt("Enter Your element to an  array: ")
+//     var e = [a, b, c, d]
+//     alert('Array becomes: ' + e)
+// } else if (f === "2") {
+//     var s = prompt("Enter Your Name: ")
+//     alert("Name: " + s)
+//     var p = prompt("Enter Your Roll No: ")
+//     alert("Rollno: " + p)
 
-    var g = +prompt("Enter Your Maths Marks: ");
-    if (g >= 33) {
-        alert("You've Passed this Subject");
+//     var g = +prompt("Enter Your Maths Marks: ");
+//     if (g >= 33) {
+//         alert("You've Passed this Subject");
 
-    } else {
-        alert("You havent cleared")
-    }
+//     } else {
+//         alert("You havent cleared")
+//     }
 
-    var h = +prompt("Enter Your Chemistry Marks: ");
-    if (h >= 33) {
-        alert("You've Passed this Subject");
+//     var h = +prompt("Enter Your Chemistry Marks: ");
+//     if (h >= 33) {
+//         alert("You've Passed this Subject");
 
-    } else {
-        alert("You havent cleared")
-    }
+//     } else {
+//         alert("You havent cleared")
+//     }
 
-    var i = +prompt("Enter Your Physics Marks: ");
-    if (i >= 33) {
-        alert("You've Passed this Subject");
+//     var i = +prompt("Enter Your Physics Marks: ");
+//     if (i >= 33) {
+//         alert("You've Passed this Subject");
 
-    } else {
-        alert("You havent cleared")
-    }
+//     } else {
+//         alert("You havent cleared")
+//     }
 
-    var j = +prompt("Enter Your English Marks: ");
-    if (j >= 33) {
-        alert("You've Passed this Subject");
+//     var j = +prompt("Enter Your English Marks: ");
+//     if (j >= 33) {
+//         alert("You've Passed this Subject");
 
-    } else {
-        alert("You havent cleared")
-    }
+//     } else {
+//         alert("You havent cleared")
+//     }
 
-    var k = +prompt("Enter Your Urdu Marks: ");
-    var n = g + h + i + j + k;
-    var l = alert("Total becomes: " + "" + n);
-    var m = n / 500 * 100;
-    var o = alert("Your Secured: " + m + "%");
-    if (m <= 90 && m >= 80) {
-        alert("Congrats! You've got A+")
-    } else if (m < 80 && m >= 70) {
-        alert("Congrats! You've got A")
+//     var k = +prompt("Enter Your Urdu Marks: ");
+//     var n = g + h + i + j + k;
+//     var l = alert("Total becomes: " + "" + n);
+//     var m = n / 500 * 100;
+//     var o = alert("Your Secured: " + m + "%");
+//     if (m <= 90 && m >= 80) {
+//         alert("Congrats! You've got A+")
+//     } else if (m < 80 && m >= 70) {
+//         alert("Congrats! You've got A")
 
-    } else if (m < 70 && m >= 60) {
-        alert("Regards: Unsatisfactory Result" + "\n" + "Result: You've got B Grade")
+//     } else if (m < 70 && m >= 60) {
+//         alert("Regards: Unsatisfactory Result" + "\n" + "Result: You've got B Grade")
 
-    } else if (m < 60 && m >= 50) {
-        alert("Regards: Unsatisfactory Result" + "\n" + "Result: You've got C Grade")
+//     } else if (m < 60 && m >= 50) {
+//         alert("Regards: Unsatisfactory Result" + "\n" + "Result: You've got C Grade")
 
-    } else if (m < 50 && m >= 45) {
-        alert("Regards: Unsatisfactory Result" + "\n" + "Result: You've got D Grade")
-    } else if (m < 45 && m >= 44) {
-        alert("Regards: Unsatisfactory Result" + "\n" + "Result: You've got E Grade")
+//     } else if (m < 50 && m >= 45) {
+//         alert("Regards: Unsatisfactory Result" + "\n" + "Result: You've got D Grade")
+//     } else if (m < 45 && m >= 44) {
+//         alert("Regards: Unsatisfactory Result" + "\n" + "Result: You've got E Grade")
 
-    } else {
-        alert("Alas! You've Failed")
-    }
+//     } else {
+//         alert("Alas! You've Failed")
+//     }
 
-    alert("🅼🅰🆁🅺🆂🅷🅴🅴🆃" + "\n" + "\n" + "Name: " + s + "\n" + "Roll no: " + p + "\n" + "Maths: " + g + "\n" + "Chemistry: " + h + "\n" + "Physics: " + i + "\n" + "English: " + j + "\n" + "Urdu: " + k + "\n" + "Total Marks: " + n + "/600" + "\n" + "Percentage: " + m + "%")
-} else {
+//     alert("🅼🅰🆁🅺🆂🅷🅴🅴🆃" + "\n" + "\n" + "Name: " + s + "\n" + "Roll no: " + p + "\n" + "Maths: " + g + "\n" + "Chemistry: " + h + "\n" + "Physics: " + i + "\n" + "English: " + j + "\n" + "Urdu: " + k + "\n" + "Total Marks: " + n + "/600" + "\n" + "Percentage: " + m + "%")
+// } else {
 
 
 
-    alert("You've exited the program");
-}
+//     alert("You've exited the program");
+// }
